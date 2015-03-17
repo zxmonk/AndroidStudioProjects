7a00cfc60fa77d872f2c774f1556d59ac560e62c
zxmonk <zxmonk@163.com>
2015-03-17 16:38:18 +0800

    002

--- HelloWorld/app/src/main/java/com/zxmonk/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/com/zxmonk/helloworld/HelloWorldActivity.java
@@ -2,8 +2,13 @@ package com.zxmonk.helloworld;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class HelloWorldActivity extends ActionBarActivity {
@@ -11,7 +16,20 @@ public class HelloWorldActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE)
+        Log.d("HelloWorldActivity","onCreate execute");
         setContentView(R.layout.activity_hello_world);
+
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HelloWorldActivity.this,"You clicked Button 1",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 
@@ -27,13 +45,27 @@ public class HelloWorldActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /*int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);*/
+
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+
+        }
+        return true;
+
+
     }
 }
