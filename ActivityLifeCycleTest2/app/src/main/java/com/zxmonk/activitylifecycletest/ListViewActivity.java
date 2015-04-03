78bf74bc055cef99075fdb1babfc68b9d0f286c7
zxmonk <zxmonk@163.com>
2015-04-03 16:13:24 +0800

    201504031613

--- ActivityLifeCycleTest2/app/src/main/java/com/zxmonk/activitylifecycletest/ListViewActivity.java
+++ ActivityLifeCycleTest2/app/src/main/java/com/zxmonk/activitylifecycletest/ListViewActivity.java
@@ -2,23 +2,38 @@ package com.zxmonk.activitylifecycletest;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ListViewActivity extends BaseActivity {
 
-    private String[] data = { "Apple", "Banana", "Orange", "Watermelon",
-            "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango"};
+    private List<Fruit> fruitList = new ArrayList<Fruit>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+        initFruits(); // 初始化水果数据
+        FruitAdapter adapter = new FruitAdapter(ListViewActivity.this,
+                R.layout.fruit_item, fruitList);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ListViewActivity.this,android.R.layout.simple_list_item_1, data);
         ListView listView = (ListView) findViewById(R.id.list_View);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(ListViewActivity.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
@@ -43,4 +58,90 @@ public class ListViewActivity extends BaseActivity {
 
         return super.onOptionsItemSelected(item);
     }*/
+
+
+    private void initFruits() {
+        Fruit apple = new Fruit("Apple", R.drawable.apple);
+        fruitList.add(apple);
+        Fruit banana = new Fruit("Banana", R.drawable.banana);
+        fruitList.add(banana);
+        Fruit orange = new Fruit("Orange", R.drawable.orange);
+        fruitList.add(orange);
+        Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon);
+        fruitList.add(watermelon);
+        Fruit pear = new Fruit("Pear", R.drawable.pear);
+        fruitList.add(pear);
+        Fruit grape = new Fruit("Grape", R.drawable.grape);
+        fruitList.add(grape);
+        Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple);
+        fruitList.add(pineapple);
+        Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry);
+        fruitList.add(strawberry);
+        Fruit cherry = new Fruit("Cherry", R.drawable.cherry);
+        fruitList.add(cherry);
+        Fruit mango = new Fruit("Mango", R.drawable.mango);
+        fruitList.add(mango);
+
+        fruitList.add(apple);
+
+        fruitList.add(banana);
+
+        fruitList.add(orange);
+
+        fruitList.add(watermelon);
+
+        fruitList.add(pear);
+
+        fruitList.add(grape);
+
+        fruitList.add(pineapple);
+
+        fruitList.add(strawberry);
+
+        fruitList.add(cherry);
+
+        fruitList.add(mango);
+        fruitList.add(mango);
+
+        fruitList.add(apple);
+
+        fruitList.add(banana);
+
+        fruitList.add(orange);
+
+        fruitList.add(watermelon);
+
+        fruitList.add(pear);
+
+        fruitList.add(grape);
+
+        fruitList.add(pineapple);
+
+        fruitList.add(strawberry);
+
+        fruitList.add(cherry);
+
+        fruitList.add(mango);
+        fruitList.add(mango);
+
+        fruitList.add(apple);
+
+        fruitList.add(banana);
+
+        fruitList.add(orange);
+
+        fruitList.add(watermelon);
+
+        fruitList.add(pear);
+
+        fruitList.add(grape);
+
+        fruitList.add(pineapple);
+
+        fruitList.add(strawberry);
+
+        fruitList.add(cherry);
+
+        fruitList.add(mango);
+    }
 }
